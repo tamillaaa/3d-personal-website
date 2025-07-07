adefa62b1bc8cc43e90d75443c32bac4b6378e6f
Tamilla Zeynalova <tzeynalovaa@gmail.com>
2025-07-07 09:51:54 +0400

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "3d-portfolio-website", // <-- 👈 add this line
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
 });
